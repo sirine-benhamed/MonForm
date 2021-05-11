513ca81c0f26563b2630a0a584cfcc47f1ba6153
Your Name <benhamed.sirin@myu.universitecentrale.tn>
2021-05-11 20:03:51 +0200

    cc

--- login.php
+++ login.php
@@ -10,20 +10,11 @@
 <body>
 <div class="container">
     <form action="processLogin.php" methode="post"> 
-       username:<input name="user" type="text" class="form-control">
-       password:<input name="pwd" type="password" class="form-control">
-       <input type="submit" class="btn btn-primary">
+    username:<input name="username" type="text" class="form-control">
+    password:<input name="pwd" type="password" class="form-control">
+    <input type="submit" class="btn btn-primary">
     </form>
-    <?php 
-    //si ce message (variable) ce trouve dans le tab get qui sera afficher dans l'url
-            if(isset($_GET['errorMessage'])){
-      ?>
-    <div class="alert alert-danger">
-       <?=$_GET['errorMessage'] ?>
-    </div>
-    <?php 
-    }
-    ?>
+  
     </div>
     
 </body>
--- processLogin.php
+++ processLogin.php
@@ -1,18 +1,13 @@
 <?php
-//tester si le login et le mot de passe sont correct
-//username:aymen et le mot de passe secret
-/* echo '<br> contenu de $_GET : <br>';
-var_dump($_GET);
-echo '<br> contenu de $_post : <br>';
-var_dump($_POST); */
-if($_POST['user']=='sirine' && $_POST['pwd']=='123') {
-    //rediriger le user vers sa page d'acceil 
+if($_POST['username']=='sirine' && $_POST['pwd']=='secret')
+{
     header('location:home.php');
 }
+
 /*else{
     //rediriger le user vers la page login avec un message d'erreur
     //le ? c pour affiche le message apres le ? dans l'url
     header('location:login.php?errorMessage=veillez verifier votre login ou mot de passe');
     
-}
-?>*/
+}*/
+?>
